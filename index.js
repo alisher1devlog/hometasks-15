fd51fd845e1bed8c4a83e1325e0784e9194d551f
Alisher <alisheryondoshaliyev77@gmail.com>
2025-09-04 13:25:50 +0500

    some update

---
+++ index.js
@@ -0,0 +1,7 @@
+const users=[
+    {username:"begzodbek",email:"begzodbek@gmail.com",password:"bibilan28"},
+    {username:"alisher",email:"alisher@gmail.com",password:"alisher25"},
+    {username:"jasur",email:"jasur2005@gmail.com",password:"jasur123"}
+]
+
+const 
